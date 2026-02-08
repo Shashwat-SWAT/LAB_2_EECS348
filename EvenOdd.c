7d3d379cf46ab19be886553b47cf0a5cb3c54f7e
Shashwat_ghimire <67894307+Shashwat-SWAT@users.noreply.github.com>
2026-02-08 17:26:15 -0600

    Defining the function

--- EvenOdd.c
+++ EvenOdd.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include "EvenOdd.h"
 
-int is_odd_even()
+int is_odd_even(int num)
 {
-    //
+    if (num % 2 == 0)
+    {
+        return "Even";
+    }
+    else
+    {
+        return "Odd";
+    }
 }
--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+
 int main()
 {
-    //
+    int number;
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    printf("ok!");
 }
